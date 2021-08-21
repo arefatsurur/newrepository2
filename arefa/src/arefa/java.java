f6430c5d98066ecc9d22a47c0c270cffa89f83ed
ariftech <87348133+arefatsurur@users.noreply.github.com>
2021-08-21 11:08:48 -0400

    hi

---
+++ arefa/src/arefa/java.java
@@ -0,0 +1,11 @@
+package arefa;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("hi arefa");
+		System.out.println ("hi ");
+	}
+
+}
